4bc70adc0b2e8fefbdb83b3874773302a4850663
ALEKSEY KAPUSTYANENKO <lehkap@hotmail.com>
2020-04-04 16:29:01 +0400

    Add change log tracker

---
+++ NihFix.PgProfiler.LogProcessing/LogChangeTracker.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace NihFix.PgProfiler.LogProcessing
+{
+    /// <summary>
+    /// Inform about new record in log;
+    /// </summary>
+    public class LogChangeTracker : IDisposable
+    {
+        private string _logFolderPath;
+        private readonly FileSystemWatcher _fileSystemWatcher;
+        private readonly Dictionary<string, long> _streamPositionDictionary = new Dictionary<string, long>();
+        
+        /// <summary>
+        /// Occurs when new data adds to log.
+        /// </summary>
+        public event EventHandler<OnLogAddEventArgs> OnLogChange;
+
+        /// <summary>
+        /// Ctr.
+        /// </summary>
+        /// <param name="logFolderPath">Path to folder with log files.</param>
+        public LogChangeTracker(string logFolderPath)
+        {
+            _logFolderPath = logFolderPath;
+            _fileSystemWatcher = new FileSystemWatcher();
+            _fileSystemWatcher.Path = logFolderPath;
+            _fileSystemWatcher.Filter = "*.log";
+            _fileSystemWatcher.NotifyFilter = NotifyFilters.CreationTime |
+                                              NotifyFilters.FileName |
+                                              NotifyFilters.LastWrite;
+            _fileSystemWatcher.Changed += FileSystemWatcherOnChanged;
+            _fileSystemWatcher.EnableRaisingEvents = true;
+        }
+
+        private void FileSystemWatcherOnChanged(object sender, FileSystemEventArgs e)
+        {
+            using var fileStream = new FileStream(e.FullPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            fileStream.Position = GetCurrentStreamPosition(e.FullPath);
+            using var streamReader = new StreamReader(fileStream);
+            OnLogChange?.Invoke(this, new OnLogAddEventArgs(streamReader.ReadToEnd(), e.FullPath));
+            SaveCurrentStreamPosition(e.FullPath,fileStream.Position);
+        }
+
+        private long GetCurrentStreamPosition(string filePath)
+        {
+            if (_streamPositionDictionary.TryGetValue(filePath, out var position))
+            {
+                return position;
+            }
+
+            _streamPositionDictionary.Add(filePath, 0);
+            return 0;
+        }
+
+        private void SaveCurrentStreamPosition(string filePath, long position)
+        {
+            _streamPositionDictionary[filePath]=position;
+        }
+
+
+        ///<inheritdoc/>
+        public void Dispose()
+        {
+            _fileSystemWatcher?.Dispose();
+        }
+    }
+}
---
+++ NihFix.PgProfiler.LogProcessing/OnLogAddEventArgs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace NihFix.PgProfiler.LogProcessing
+{
+    /// <summary>
+    /// Event args for LogReader.OnLogChange event.
+    /// </summary>
+    public class OnLogAddEventArgs:EventArgs
+    {
+        /// <summary>
+        /// New data.
+        /// </summary>
+        public string NewData { get; }
+        
+        /// <summary>
+        /// Path to log file.
+        /// </summary>
+        public string FilePath { get; }
+
+        public OnLogAddEventArgs()
+        {
+            
+        }
+
+        /// <summary>
+        /// Ctr.
+        /// </summary>
+        /// <param name="newData">New log data.</param>
+        /// <param name="filePath">Path to log file.</param>
+        public OnLogAddEventArgs(string newData, string filePath)
+        {
+            NewData = newData;
+            FilePath = filePath;
+        }
+    }
+}
