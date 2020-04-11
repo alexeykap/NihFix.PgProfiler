9ab167feba048414ad3d7391e66150ff35784cab
ALEKSEY KAPUSTYANENKO <lehkap@hotmail.com>
2020-04-11 12:51:36 +0400

    commit uncommited

---
+++ NihFix.PgProfiler.Sample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NihFix.PgProfiler.LogProcessing;
+
+namespace NihFix.PgProfiler.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var logReader = new LogChangeTracker(@"C:\Program Files\PostgreSQL\10\data\log");
+            logReader.OnLogChange+=LogReaderOnOnLogChange;
+            while (Console.ReadLine()!="q")
+            {
+                
+            }
+
+            logReader.OnLogChange -= LogReaderOnOnLogChange;
+        }
+
+        private static void LogReaderOnOnLogChange(object? sender, OnLogAddEventArgs e)
+        {
+            Console.WriteLine(e.FilePath);
+            Console.Write(e.NewData);
+        }
+    }
+}
